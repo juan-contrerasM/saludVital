0d57e3198d7e423d5e779bbef84e03f00b38d293
JUANCONTRERAS <juanj.contrerasm@uqvirtual.edu.co>
2025-10-13 11:26:51 -0500

    Se modica coneccion bd

--- backend/db/coneccion.py
+++ backend/db/coneccion.py
@@ -1,16 +1,12 @@
-import os
 from dotenv import load_dotenv
+import os
 import psycopg2
 
+if os.path.exists(".env"):
+    load_dotenv()  # solo carga .env si existe
 
-### Conexión a la base de datos
-def conectar_db():
-    # 1) Cargar variables
-    load_dotenv()
-    url = os.getenv("DATABASE_URL")
-    
-    if not url:
-        raise ValueError("La variable de entorno DATABASE_URL no está definida.")
-    
-    conn= psycopg2.connect(url, sslmode="require")
-    return conn
+url = os.getenv("DATABASE_URL")
+if not url:
+    raise ValueError("La variable de entorno DATABASE_URL no está definida.")
+
+conn = psycopg2.connect(url, sslmode="require")
